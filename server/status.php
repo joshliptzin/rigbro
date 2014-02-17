7f2b9b9d51a1a990ba26e901bdffd4c72ca4eaae
Josh Liptzin <joshliptzin@gmail.com>
2014-02-17 12:28:48 -0500

    Removed RigBro header

--- server/status.php
+++ server/status.php
@@ -50,7 +50,6 @@ $in_alarm_state = $mhs_alarm || $dead_rig_alarm;
 </head>
 <body>
 
-<h2>RigBro Array</h2>
 <!-- Low MH/s Alert -->
 <div class="<?= $mhs_alarm ? "alarm" : "" ?>">
     <h3>Total MH/s: <?= $total_mhs ?> (expected: <?= $expected_mhs ?> MH/s)</h3>
